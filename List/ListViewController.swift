86faefe7cd46e44bd065808388656adefa7df987
Christopher <christopher@christophers-macbook.local>
2018-06-05 13:15:32 -0700

    Added Persistent data and working

--- List/ListViewController.swift
+++ List/ListViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
-    let itemArray = ["eggs","milk"]
-
+    var itemArray = ["eggs","milk"]
+    
+let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         
 
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "ListArray") as? [String]{
+            itemArray = items}
     }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,16 +36,36 @@ class ListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
+        
+        
         if  ( tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark ) {         tableView.cellForRow(at: indexPath)?.accessoryType = .none} else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
 
 }
-        
-
-        
-        
-        tableView.deselectRow(at: indexPath, animated: true)
+        tableView.deselectRow(at: indexPath, animated: true)}
     
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+        
+            self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ListArray")
+            
+            self.tableView.reloadData()
+            
+            //what hpappens when clicked
 
-    }}
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item Here"
+            textField = alertTextField
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
 
